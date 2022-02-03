13532d80f8a47b79c541157f4381882f87beab07
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-03 23:49:30 +0200

    Updating the base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import CartProvider from './store/CartProvider';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 import './App.css';
 
 function App() {
+  const [cartIsEnabled, setcartIsEnabled] = useState(false);
+
+  const clickCartHandler = () => {
+    setcartIsEnabled(true);
+  };
+
+  const hideCartHandler = () => {
+    setcartIsEnabled(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+      {cartIsEnabled && <Cart onHideCart={hideCartHandler} />}
+      <Header onCartClick={clickCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
