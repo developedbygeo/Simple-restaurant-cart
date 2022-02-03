39098d0310a162505ee2a2086df3bf8a68c4db7d
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-04 00:06:08 +0200

    Adding the parent component which renders the MealsText and CurrentMeals components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsText from './MealsText';
+import CurrentMeals from './CurrentMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsText />
+      <CurrentMeals />
+    </>
+  );
+};
+
+export default Meals;
