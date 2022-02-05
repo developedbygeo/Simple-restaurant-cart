0ee73f6daedead3f05e215834e58875b10b69b6e
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-05 21:33:59 +0200

    Adding thhe CartItem component which renders the selected items, along with the add/remove buttons

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,23 @@
+import styles from './CartItem.module.css';
+
+const CartItem = ({ name, price, amount, onAdd, onRemove }) => {
+  const parsedPrice = `$${price.toFixed(2)}`;
+
+  return (
+    <li className={styles['cart-item']}>
+      <div className={styles.qtInfo}>
+        <h2>{name}</h2>
+        <div className={styles.summary}>
+          <span className={styles.price}>{parsedPrice}</span>
+          <span className={styles.amount}>x {amount}</span>
+        </div>
+      </div>
+      <div className={styles.actions}>
+        <button onClick={onRemove}>−</button>
+        <button onClick={onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
